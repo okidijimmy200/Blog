13bd7193e9784fd20401974ea9d38f1c9fa9dbee
okidijimmy200 <okidijimmie@gmail.com>
2020-04-28 22:55:09 +0300

    completed the views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import POST
 
-# Create your views here.
+
+def post_list(request):
+    '''retrieving all the posts with the published status using the published manager'''
+    posts = POST.published.all()
+    return render(request,
+    'blog/post/list.html',
+    {'posts': posts})
+
+
+'''view to display a single post'''
+def post_detail(request, year, month, day, post):
+    ''' get_object_or_404 retrieves the object that matches the given parameters or
+launches an HTTP 404 '''
+    post = get_object_or_404(POST, slug=post,
+    status='published',
+    publish__year=year,
+    publish__month=month,
+    publish__day=day)
+    return render(request,
+    'blog/post/detail.html',
+    {'post': post})
